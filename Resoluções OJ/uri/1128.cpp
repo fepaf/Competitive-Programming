448ff7e8894b86061fb340b831aee9e64b86efdd
fepaf <fepaf.eng@uea.edu.br>
2017-12-27 13:01:53 -0400

    Novas soluções

---
+++ Resoluções OJ/uri/1128.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);
+#define UNVISITED -1
+#define VISITED 1
+#define vi vector<int>
+#define vvi vector<vi>
+#define eb emplace_back
+
+using namespace std;
+
+vi num,s;
+vvi adj,adjt;
+int n,m,u,v,p,nscc;
+
+void dfs(int u, int pass){
+    num[u] = VISITED;
+    vi vizinhos = pass ? adj[u] : adjt[u];
+    for (int v : vizinhos){
+        if (num[v]==UNVISITED){
+            dfs(v,pass);
+        }
+    }
+    s.eb(u);
+}
+
+int main(){_
+    while ((cin>>n>>m)&&(n||m)){
+        adj = vvi(n+1); adjt = vvi(n+1);
+        while (m--){
+            cin>>u>>v>>p;
+            adj[u].eb(v);
+            adjt[v].eb(u);
+            if (p==2){
+                adj[v].eb(u);
+                adjt[u].eb(v);
+            }
+        }
+        num.assign(n+1,UNVISITED); s.clear();
+        for (int i=1; i<=n; i++){
+            if (num[i]==UNVISITED){
+                dfs(i,1);
+            }
+        }
+        num.assign(n+1,UNVISITED); nscc=0;
+        for (int i=n; i>=1; i--){
+            if (num[s[i-1]]==UNVISITED){
+                dfs(s[i-1],0);
+                ++nscc;
+            }
+        }
+        cout<<(nscc==1)<<endl;
+    }
+    return 0;
+}
---
+++ Resoluções OJ/uri/1147.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);
+#define UNVISITED -1
+#define VISITED 1
+#define vi vector<int>
+#define vvi vector<vi>
+#define eb emplace_back
+
+using namespace std;
+
+int ans,teste,a,b;
+string moves,peao,cavalo;
+vvi tab;
+
+int main(){_
+    while ((cin>>cavalo)&&(cavalo!="0")){
+        tab = vvi(10,vi(10,1));
+        for (int i=8;i--;){
+            cin>>peao;
+            tab[peao[0]-'0'-1][peao[1]-'a'] = 0;
+            tab[peao[0]-'0'-1][peao[1]-'a'+2] = 0;
+        }
+        a = cavalo[0]-'0'; b = cavalo[1]-'a'+1;
+        ans = 0;
+        for (int i=1; i<=8; i++){
+            for (int j=1; j<=8; j++){
+                ans += ((a-i)*(a-i)+(b-j)*(b-j)==5)*tab[i][j];
+            }
+        }
+        cout<<"Caso de Teste #"<<++teste<<": "<<ans<<" movimento(s)."<<endl;
+    }
+    return 0;
+}
