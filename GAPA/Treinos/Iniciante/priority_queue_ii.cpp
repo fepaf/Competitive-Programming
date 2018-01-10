c6fd5948e58de95596f3afd65d06141fc0535427
fepaf <francisco.fepaf@gmail.com>
2018-01-10 14:02:37 -0400

    priority_queue com par e vector

--- GAPA/Treinos/Iniciante/priority_queue_ii.cpp
+++ GAPA/Treinos/Iniciante/priority_queue_ii.cpp
@@ -1,30 +1,31 @@
 #include<bits/stdc++.h>
 #define _ ios_base::sync_with_stdio(0);
 #define i64 long long
+#define ii pair<int,int>
 #define fs first
 #define sc second
 #define eb emplace_back
 #define ALL(x) (x).begin() , (x).end()
 
-using namespace std;
+using namespace std;//
 
-int x, n, op;
-priority_queue<int, vector<int>, greater<int> > pq;
+int x, y, n, op;
+priority_queue<ii, vector<ii>, greater<ii> > pq;// sempre tira o menor par
 
 int main(){_
     while (cin>>n){
         while (n--){
             cin>>op;
             if (op==1){
-                cin>>x;
-                pq.push(x);//coloca na pq
+                cin>>x>>y;
+                pq.push(ii(x, y));//coloca na pq
             }
             else{
                 if (!pq.empty()){
-                    cout<<"->"<<pq.top()<<endl;
-                    pq.pop();
+                    ii par = pq.top(); pq.pop();
+                    cout<<"->"<<(par.fs)<<" - "<<(par.sc)<<endl;
                 }
-                cout<<"tamaho: "<<pq.size()<<endl;
+                cout<<"tamanho: "<<pq.size()<<endl;
             }
         }
     }
