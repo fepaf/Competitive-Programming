7fc8c6782fcdcc931e7c817d560b9390264c2a30
fepaf <francisco.fepaf@gmail.com>
2018-12-25 19:50:55 -0400

    CIRCLEDIV accepted submission

--- Resoluções OJ/spoj/circlediv.cpp
+++ Resoluções OJ/spoj/circlediv.cpp
@@ -7,6 +7,6 @@ int main(){
     for (i=1, scanf("%i",&t); i<=t; i++){
         scanf("%lli",&n);
 
-        printf("Case %i: %lli\n",i,(((n*(n-1))%MOD)*500000004)%MOD + (((((((n*(n-1))%MOD)*(n-2))%MOD)*(n-3))%MOD)*41666667)%MOD +1);
+        printf("Case %i: %lli\n",i,((((n*(n-1))%MOD)*500000004)%MOD + (((((((n*(n-1))%MOD)*(n-2))%MOD)*(n-3))%MOD)*41666667)%MOD +1)%MOD);
     }
 }
