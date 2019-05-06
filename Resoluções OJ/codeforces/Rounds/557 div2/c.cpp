5899226bb7ca9ac4a8433d67422dca7f03724c11
Francisco Arcos Filho <fepaf.eng@uea.edu.br>
2019-05-06 10:46:26 -0400

    Versao ACCEPTED, com vector no lugar do map

--- Resoluções OJ/codeforces/Rounds/557 div2/c.cpp
+++ Resoluções OJ/codeforces/Rounds/557 div2/c.cpp
@@ -7,44 +7,39 @@
 #define all(x) (x).begin(),(x).end()
 #define eb emplace_back
 #define vi vector<int>
+#define vvi vector<vi>
 #define sz(x) ((int)(x).size())
 #define fs first
 #define sc second
 
 using namespace std;
 
+
 int x[MAX], n, k;
 i64 ans;
-map<int, vi> pos;
+vvi pos;
 
 int main(){_
 	while (cin >> n >> k){
 		ans = 3*n-2;
 		
-
+		pos = vvi(n+2);
 		for (int i=0; i<k; ++i){
 			cin >> x[i];
 			pos[x[i]].eb(i);
 		}
 		
-		for (auto elem : pos){
-			int chave = elem.first;
-			int primeiro = pos[chave][0];
-			
-			cout << chave << ": ";
-			for (auto x : pos[chave]){
-				cout << x << " ";
-			}
-			cout << endl;
-			
-			//ans -= (lower_bound(all(pos[chave-1]), primeiro) != pos[chave-1].end());
-			//ans -= (lower_bound(all(pos[chave+1]), primeiro) != pos[chave+1].end());
-			
-			ans -= 1;
+		for (int chave=1; chave<=n; ++chave){
+			if (sz(pos[chave]) != 0){
+				--ans;
+				int primeiro = pos[chave][0];
+				ans -= (upper_bound(all(pos[chave-1]), primeiro) != pos[chave-1].end());
+				ans -= (upper_bound(all(pos[chave+1]), primeiro) != pos[chave+1].end());
 				
-		}		
+			}
+		}	
+			
 		cout << ans << endl;
-		pos.clear();
 	}
 
 	return 0;
