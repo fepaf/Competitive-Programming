b6749d5c5aadc172da3e4e64254d4f2342c5453f
Francisco Arcos Filho <fepaf.eng@uea.edu.br>
2019-09-20 02:35:50 -0400

    Problem D, WA

--- Resoluções OJ/codeforces/Rounds/586 div2/d.cpp
+++ Resoluções OJ/codeforces/Rounds/586 div2/d.cpp
@@ -10,7 +10,7 @@
 #define fs first
 #define sc second
 #define eb emplace_back
-#define vi vector<int>
+#define vi vector<i64>
 #define vvi vector<vi>
 #define vii vector<ii>
 #define vvii vector<vii>
@@ -23,8 +23,29 @@ i64 n, b[MAX];
 
 int main(){_
 	while (cin >> n){
+		vi odd, even;
 		for (int i=0; i<n; ++i){
 			cin >> b[i];
+			if (b[i]&1LL){
+				odd.eb(b[i]);
+			}
+			else{
+				even.eb(b[i]);
+			}
+		}
+		if (sz(odd) < sz(even)){
+			cout << sz(odd) << endl;
+			for (auto x : odd){
+				cout << x << ' ';
+			}
+			cout << endl;
+		}
+		else {
+			cout << sz(even) << endl;
+			for (auto x : even){
+				cout << x << ' ';
+			}
+			cout << endl;
 		}
 	}
 	return 0;
