c291eaa0a9bf19d4a73dd7c497db74cd04866a02
Francisco Arcos Filho <francisco.fepaf@gmail.com>
2020-01-10 13:43:14 -0400

    Problem A, Accepted

---
+++ Resoluções OJ/codeforces/Rounds/613 div2/a.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define rep(i,a,b) for (int (i)=(a); (i)<=(b); ++(i))
+#define repi(i,a,b) for (int (i)=(a); (i)>=(b); --(i))
+
+using namespace std;
+
+int n, cl, cr;
+string s;
+
+int main(){_
+    cin >> n >> s;
+    int t = sz(s);
+    for (int i=0; i<t; ++i){
+        cl += (s[i] == 'L');
+        cr += (s[i] == 'R');
+    }
+    cout << (cl+cr +1) << endl;
+	return 0;
+}
