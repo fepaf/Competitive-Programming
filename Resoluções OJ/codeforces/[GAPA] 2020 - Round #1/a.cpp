473cf3ba926e22285b6d773a8de7c4311a31ba4e
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-03-05 08:26:07 -0400

    Problem A, Accepted

---
+++ Resoluções OJ/codeforces/[GAPA] 2020 - Round #1/a.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+
+using namespace std;
+
+int n, a, b, x;
+
+int main(){_
+    cin >> n;
+    for (int i=0; i<n; ++i){
+        cin >> x;
+        a |= x;
+    }
+    for (int i=0; i<n; ++i){
+        cin >> x;
+        b |= x;
+    }
+    cout << (a+b) << endl;
+    return 0;
+}
