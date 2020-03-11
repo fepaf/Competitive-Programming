b2dc84f3fe16fc32153df635c1522bffe92e32b8
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2020-03-11 07:48:59 -0400

    Problem K, Wrong Answer, Upsolving

---
+++ Resoluções OJ/codeforces/2016, IX Samara Regional Intercollegiate Programming Contest/k.py
@@ -0,0 +1,23 @@
+s = input()
+r = s[::-1]
+n = len(s)
+p=0
+for i in range(n):
+    if (s[i]!=r[i]):
+        p=i
+        break
+s1 = s[:p]+s[p+1:]
+r1 = s1[::-1]
+s2 = s[:n-1-p]+s[n-p:]
+r2 = s2[::-1]
+if (r==s and n&1):
+    print("YES")
+    print(n//2+1)
+elif (r1==s1):
+    print("YES")
+    print(p+1)
+elif (r2==s2):
+    print("YES")
+    print(n-p)
+else:
+    print("NO")
