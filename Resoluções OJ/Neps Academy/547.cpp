b682e754ec72525d97621a4cd9cd5e94236a82e0
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2020-04-17 16:57:05 -0400

    Dança Indígena, Accepted

--- Resoluções OJ/Neps Academy/547.cpp
+++ Resoluções OJ/Neps Academy/547.cpp
@@ -2,7 +2,7 @@
 #define _ ios_base::sync_with_stdio(0);
 #define endl '\n'
 #define INF 0x3f3f3f3f
-#define MAX (3000123)
+#define MAX (1<<20)
 #define i64 long long
 #define all(x) (x).begin() , (x).end()
 #define sz(x) (int)(x).size()
@@ -19,26 +19,74 @@
 
 using namespace std;
 
-int n, e, v, d;
-int ant[MAX], hor[MAX], p1[MAX];
+int n, e, v, d, dir[2][MAX];
+string hor, ant;
+vi ans[3], p;
+
+vi p_function(string t){
+    int n = sz(t);
+    vi p = vi(n);
+    p[0] = 0;
+    for (int i=1; i<n; i++){
+        p[i] = p[i-1];
+        while (t[i] != t[p[i]] && p[i]) p[i] = p[p[i]-1];
+        p[i] += (t[i] == t[p[i]]);
+    }
+    return p;
+}
 
 int main(){_
+
 	cin >> n >> e;
-	for (int i=0; i<e; ++i){
-		cin >> v >> e;
-		if (~d){
-			hor[v-1] = hor[v+n] = hor[v+n+n] = 1;
-		}
-		else {
-			ant[v-1] = ant[v+n] = ant[v+n+n] = 1
+	while (e--){
+		cin >> v >> d;
+		dir[~d ? 0 : 1][v-1] = 1;
+	}
+
+	for (int i=0; i<n; ++i){
+		hor += (dir[0][i] ? '1' : '0');
+		ant += (dir[1][i] ? '1' : '0');
+	}
+
+	p = p_function(hor+"#"+ant+ant);
+
+	for (int i=2*n+1; i<=3*n; ++i){
+		if (p[i] == n){
+			ans[0].eb(i-2*n);
 		}
 	}
-	hor[n] = ant[n] = INF;
-	p1[0] = 0;
-	for (int i=1; i<n; ++i){
-		p1[i] = p1[i-1];
-		while (hor[i] != hor[p1[i]] && p1[i]) p1[i] = p1[p1[i]-1];
-		p1[i] = (hor[i] == hor[p1[i]]);
+	
+	p = p_function(hor+"#"+hor+hor);
+
+	for (int i=2*n+1; i<=3*n; ++i){
+		if (p[i] == n){
+			ans[1].eb(i-2*n);
+		}
 	}
+	
+	p = p_function(ant+"#"+ant+ant);
+
+	for (int i=2*n+1; i<=3*n; ++i){
+		if (p[i] == n){
+			ans[2].eb(i-2*n);
+		}
+	}
+
+	if (sz(ans[2])==n && ant[0]=='0'){
+		cout << ans[1][0] << endl;
+		return 0;
+	}
+
+	if (sz(ans[1])==n && hor[0]=='0'){
+		cout << ans[2][0] << endl;
+		return 0;
+	}
+
+	if (sz(ans[0])){
+		cout << ans[0][0] << endl;
+		return 0;
+	}
+
+	cout << n << endl;
     return 0;
 }
