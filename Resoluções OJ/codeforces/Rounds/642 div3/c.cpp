f719ff8304df06b2ed65e86794114dcff74feea5
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-05-14 22:27:22 -0400

    Problem C, Accepted

---
+++ Resoluções OJ/codeforces/Rounds/642 div3/c.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+
+using namespace std;
+
+int t;
+i64 n, ans;
+
+int main(){_
+    cin >> t;
+    while (t--){
+        cin >> n;
+        ans = 0;
+        for (i64 i=1; i<=n; i+=2){
+            ans += (i>>1)*(4*i-4);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
