66068e62036fcbb1e48690dfafda9a87d76e9ad7
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-06-02 00:14:29 -0400

    Problem: Mini Calculadora

---
+++ Resoluções OJ/Neps Academy/594.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+
+using namespace std;
+
+int c, d, q, m;
+
+int main(){_
+    cin >> c >> d >> q;
+    m = gcd(d,q);
+    d /= m;
+    q /= m;
+    if (d<=c && q<=c) cout << d << ' ' << q << endl;
+    else cout << "IMPOSSIVEL\n";
+    return 0;
+}
