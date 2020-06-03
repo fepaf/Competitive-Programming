4427fb46eb33cb22b67c589808fcced68b6eefd2
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-06-02 22:13:01 -0400

    Problem: Código (OBI 2015)

---
+++ Resoluções OJ/Neps Academy/47.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+
+using namespace std;
+
+int n, a[MAX], ans;
+
+int main(){_ 
+    cin >> n;
+    for (int i=0; i<n; ++i){
+        cin >> a[i];
+        ans += (i>1 && a[i-2] && !a[i-1] && !a[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
