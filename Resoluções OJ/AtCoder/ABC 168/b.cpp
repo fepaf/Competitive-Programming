c80100e8daf7901d7a1a1ce51456efcfc0502ee6
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-06-13 20:41:17 -0400

    ABC 168, B, Accepted

---
+++ Resoluções OJ/AtCoder/ABC 168/b.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+
+using namespace std;
+
+int k;
+string s;
+
+int main(){_
+    cin >> k >> s;
+    for (int i=0; i<min(sz(s),k); ++i){
+        cout << s[i];
+    }
+    if (sz(s)>k) cout << "...";
+    cout << endl;
+    return 0;
+}
