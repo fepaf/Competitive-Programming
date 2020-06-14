444b652f1864616a8f2e25e748c0ac95c29136eb
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-06-13 20:43:31 -0400

    ABC, C, Incomplete

---
+++ Resoluções OJ/AtCoder/ABC 168/c.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+
+using namespace std;
+
+int a,b,h,m;
+long double sa, sb, theta;
+const long double pi = 3.14159265358979323846264338327950288419716939937510L;
+
+int main(){_
+    printf("%.9Lf\n",M_PIl);
+    // cin >> a >> b >> h >> m;
+    if (5*h>m)theta = 2*pi*((h/12)-(m/60));
+    return 0;
+}
