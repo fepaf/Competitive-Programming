46ca442681d2a1c4981778a5aa388601107fd999
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-07-25 18:34:33 -0400

    Problem C: Accepted

---
+++ Resoluções OJ/codeforces/Rounds/656 div3/c.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+
+using namespace std;
+
+int t, n, suf, pref;
+vi a;
+
+int main(){_
+    for (cin >> t; t--;){
+        cin >> n;
+        a = vi(n);
+        for (int &ai : a){
+            cin >> ai;
+        }
+        suf = 0;
+        for (int i=n-1; i>0; i--){
+            if (a[i-1] >= a[i]){
+                ++suf;
+            }else {
+                break;
+            }
+        }
+        pref = 1;
+        for (int i=n-1-suf; i>0; i--){
+            if (a[i-1] <= a[i]){
+                ++pref;
+            }else {
+                break;
+            }
+        }
+        cout << (n-suf-pref) << endl;
+    }
+    return 0;
+}
