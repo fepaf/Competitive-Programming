1c7d8c88945e6c7f8eb9cad35aa91f4233670076
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-07-25 18:47:51 -0400

    Problem 1683: Accepted

--- Resoluções OJ/uri/1683 - Maior Retângulo em um Histograma.cpp
+++ Resoluções OJ/uri/1683 - Maior Retângulo em um Histograma.cpp
@@ -3,7 +3,7 @@
 #define endl '\n'
 #define INF 0x3f3f3f3f
 #define MAX (1<<20)
-#define MOD 1000000009
+#define MOD 1000000007
 #define i64 long long
 #define all(x) (x).begin() , (x).end()
 #define sz(x) (int)(x).size()
@@ -20,10 +20,37 @@
 #define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
 
 using namespace std;
-i64 ans, n;
+
+i64 n, h[MAX], pmd[MAX], pme[MAX], ans;
+stack<i64> p;
+
 int main(){_
     while((cin >> n) && n){
-        for ()
+        for (int i=1; i<=n; ++i){
+            cin >> h[i];
+        }
+        h[0] = h[n+1] = -1;
+        for (int i=1; i<=n+1; ++i){
+            while (!p.empty() && h[i] < h[p.top()]){
+                pmd[p.top()] = i;
+                p.pop();
+            }
+            p.push(i);
+        }
+        p.pop();
+        for (int i=n; i>=0; --i){
+            while (!p.empty() && h[i] < h[p.top()]){
+                pme[p.top()] = i;
+                p.pop();
+            }
+            p.push(i);
+        }
+        p.pop();
+        ans = 0LL;
+        for (int i=1; i<=n; ++i){
+            ans = max(ans, (pmd[i]-pme[i]-1)*h[i]);
+        }
+        cout << ans << endl;
     }
     return 0;
 }
