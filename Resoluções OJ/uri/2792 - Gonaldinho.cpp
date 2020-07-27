41401740f416e9b5038bfaeb2de5681821073db5
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-07-26 21:13:00 -0400

    Problem 2792: Accepted

---
+++ Resoluções OJ/uri/2792 - Gonaldinho.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+
+using namespace std;
+
+int n, a;
+int ft[MAX];
+
+int query(int idx){
+    int ans = 0;
+    for (; idx>0; idx-=lsb(idx)){
+        ans += ft[idx];
+    }
+    return ans;
+}
+
+void update(int idx, int val){
+    for (; idx<=n; idx+=lsb(idx)){
+        ft[idx] += val;
+    }
+}
+
+int main(){_
+    cin >> n;
+    for (int i=1; i<=n; ++i){
+        update(i,1);
+    }
+    for (int i=1; i<=n; ++i){
+        cin >> a;
+        cout << query(a) << (i!=n?' ':'\n');
+        update(a,-1);
+    }
+    return 0;
+}
