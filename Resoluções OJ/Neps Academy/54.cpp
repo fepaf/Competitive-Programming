539046e5fa17f9b81b8291c5b652a7a312c0a365
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-08-10 16:55:13 -0400

    Problem: Taco de Bilhar, Accepted

---
+++ Resoluções OJ/Neps Academy/54.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+
+using namespace std;
+
+int c, x, ans;
+set<int> cj;
+
+int main(){_
+    cin >> c;
+    while (c--){
+        cin >> x;
+        if (cj.count(x)){
+            cj.erase(x);
+        } else{
+            ans += 2;
+            cj.insert(x);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
