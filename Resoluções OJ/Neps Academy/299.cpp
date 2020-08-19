d8051f6966fc0db7dd55ce53134ce27a2081a386
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-08-19 03:29:45 -0400

    Problem: Pizza, Accepted

---
+++ Resoluções OJ/Neps Academy/299.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+// #define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 unsigned long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define pi 2*acosl(0.0)
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+
+using namespace std;
+
+int a[MAX], n, atual, ans;
+
+int main(){_
+    while (cin >> n){
+        for (int i=0; i<n; ++i){
+            cin >> a[i];
+        }
+        for (int i=0; i<n+n-1; ++i){
+            atual = max(a[i%n], atual+a[i%n]);
+            ans = max(ans, atual);
+        }
+        cout << ans << '\n';
+    }
+}
