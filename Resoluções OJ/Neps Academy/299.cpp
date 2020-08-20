4a23e5f7b827be67655293f2fb8e99b342c69b31
Francisco Elio Parente Arcos Filho (Ludus) <fepaf.eng@dmz.org.br>
2020-08-19 21:04:17 -0400

    Problem: Pizza, Accepted

--- Resoluções OJ/Neps Academy/299.cpp
+++ Resoluções OJ/Neps Academy/299.cpp
@@ -4,7 +4,7 @@
 #define INF 0x3f3f3f3f
 #define MAX (1<<20)
 #define MOD 1000000007
-#define i64 unsigned long long
+#define i64  long long
 #define all(x) (x).begin() , (x).end()
 #define sz(x) (int)(x).size()
 #define ii pair<int, int>
@@ -22,16 +22,30 @@
 
 using namespace std;
 
-int a[MAX], n, atual, ans;
+int a[MAX], n, psa[MAX], ans, me;
+deque<int> dq;
 
 int main(){_
     while (cin >> n){
-        for (int i=0; i<n; ++i){
+        for (int i=1; i<=n; ++i){
             cin >> a[i];
+            a[i+n] = a[i];
         }
-        for (int i=0; i<n+n-1; ++i){
-            atual = max(a[i%n], atual+a[i%n]);
-            ans = max(ans, atual);
+        ans = 0;
+        dq.push_back(0);
+        for (int i=1; i<=n+n-1; ++i){
+            while (!dq.empty() && dq.front() < i-n){ // < e nao <=, muito importante
+                dq.pop_front();
+            }
+
+            psa[i] = psa[i-1] + a[i];
+            ans = max(ans, psa[i]-psa[dq.front()]);
+            
+            while (!dq.empty() && psa[i] <= psa[dq.back()]){
+                dq.pop_back();
+            } 
+            dq.push_back(i);
+
         }
         cout << ans << '\n';
     }
