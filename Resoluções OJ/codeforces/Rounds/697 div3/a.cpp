e23b0f6cfafddb19f6ae400865a5cebff764c4f8
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-01-31 04:05:55 -0400

    Problem A: Accepted

---
+++ Resoluções OJ/codeforces/Rounds/697 div3/a.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+
+using namespace std;
+
+i64 t, n;
+
+int main(){_
+    for (cin >> t; t--;){
+        cin >> n;
+        cout << ((n&(n-1)) ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
