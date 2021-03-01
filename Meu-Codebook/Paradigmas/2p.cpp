8da9e2ed4647da7ffd612e826b47812557a33d10
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-03-01 17:33:19 -0400

    Exemplo 1, Dois ponteiros

---
+++ Meu-Codebook/Paradigmas/2p.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+
+using namespace std;
+
+/*
+Problem: Given an array of n integers, your task is to calculate the number of subarrays that have at most k distinct values.
+*/
+
+int l, r, n, k, a[MAX], ans;
+map<int, int> f;
+
+int main(){_
+    cin >> n >> k;
+    for (int i=0; i<n; ++i){
+        cin >> a[i];
+    }
+    l = r = 0;
+    ans = 0;
+    while (r < n){
+        if (!f.count(a[r])){
+            f[a[r]] = 0;
+        }
+        f[a[r]]++;
+
+        while (f.size() > k){
+            f[a[l]]--;
+            if (!f[a[l]]){
+                f.erase(a[l]);
+            }
+            l++;
+        }
+
+        ans += (r-l+1);
+        r++;
+    }    
+    cout << ans <<  endl;
+    return 0;
+}
