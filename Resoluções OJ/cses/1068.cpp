e5a539156960262d048cec1c77e4449f5ac0070a
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-03-28 21:44:11 -0400

    Problem 1068 - Weird Algorithm (Accepted)

---
+++ Resoluções OJ/cses/1068.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii antr<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+#define branco 0
+#define cinza -1
+#define preto 1
+
+using namespace std;
+
+/*
+Problem: Weird Algorithm
+Link: https://cses.fi/problemset/task/1068
+*/
+
+i64 n;
+
+void f(i64 n){
+    cout << n << ' ';
+    if (n==1) return;
+    if (n&1){
+        f(3*n+1);
+    } else{
+        f(n>>1);
+    }
+}
+
+int main(){_
+    cin >> n;
+    f(n);
+    cout << endl;
+    return 0;
+}
