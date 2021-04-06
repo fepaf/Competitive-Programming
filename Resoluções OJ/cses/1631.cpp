77b3e91917ddb27c753d888f353526929f06d525
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-04-06 13:57:03 -0400

    Problem 1631 - Reading Books (Accepted)

---
+++ Resoluções OJ/cses/1631.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF (2e18)
+#define MAX (100)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii antr<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+#define branco 0
+#define cinza -1
+#define preto 1
+
+using namespace std;
+
+/*
+Problem: Reading Books
+Link: https://cses.fi/problemset/task/1631
+*/
+
+i64 n, sum, ma, a;
+
+int main(){_
+    cin >> n;
+
+    for (int i=n; i--;){
+        cin >> a;
+        sum += a;
+        ma = max(ma, a);
+    }
+    cout << (ma >= sum-ma ? ma<<1 : sum) << endl;
+    return 0;     
+}
