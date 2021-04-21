996e65cb64ffa114801e018b2ef1b4ea91caae6a
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-04-20 23:43:28 -0400

    Problem 1730 - Nim Game I (Accepted)

---
+++ Resoluções OJ/cses/1730.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii antr<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+#define branco 0
+#define cinza -1
+#define preto 1
+
+using namespace std;
+
+/*
+Problem: Nim Game I
+Link: https://cses.fi/problemset/task/1730
+*/
+
+int n, ans, t, x;
+
+int main(){_
+    for (cin >>  t; t--;){
+        cin >> n;
+        ans = 0;
+        while (n--){
+            cin >> x;
+            ans ^= x;
+        }
+        cout << (ans ? "first" : "second") << endl;
+    }
+    return 0;
+}
