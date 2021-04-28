5796e5aae89af67e9d0dea0b9dc43d0126c795bc
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-04-27 20:33:10 -0400

    Problem 1072 - Two Knights (Accepted)

---
+++ Resoluções OJ/cses/1072.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+#define INF (2e18)
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+#define branco 0
+#define cinza -1
+#define preto 1
+
+using namespace std;
+
+/*
+Problem: Two Knights
+Link: https://cses.fi/problemset/task/1072
+*/
+
+int n;
+
+int main(){_
+    cin >> n;
+    for (i64 i=1; i<=n; ++i){
+        cout << (((i*i*(i*i-1))>>1)-4*(i-1)*(i-2)) << endl;
+    }
+    return 0;     
+}
