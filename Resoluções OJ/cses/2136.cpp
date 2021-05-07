d666cbad7964ecf1df73611dc802a084a5708b89
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-05-07 18:38:30 -0400

    Problem 2136 - Hamming Distance (Accepted)

---
+++ Resoluções OJ/cses/2136.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define INF (0x3f3f3f3f)
+#define MAX (1<<20)
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+#define branco 0
+#define cinza -1
+#define preto 1
+
+using namespace std;
+
+/*
+Problem: Hamming Distance
+Link: https://cses.fi/problemset/task/2136
+*/
+
+int n, k, a[MAX];
+string s;
+
+int ans;
+bitset<30> x;
+
+int main(){_
+    cin >> n >> k;
+    ans = INF;
+    for (int i=n; i--;){
+        cin >> s;
+        bitset<30> x(s);
+        a[i] = x.to_ulong();
+    }
+    for (int i=0; i<n-1; i++){
+        for (int j=i+1; j<n; ++j){
+            ans = min(ans, __builtin_popcount(a[i]^a[j]));
+        }
+    }
+    cout << ans << endl;
+    return 0;     
+}
