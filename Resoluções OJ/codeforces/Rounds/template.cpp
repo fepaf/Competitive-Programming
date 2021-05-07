fc29ca055fe74f87fa59cc6c1982e72f1043c01d
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-05-07 18:51:39 -0400

    update template

--- Resoluções OJ/codeforces/Rounds/template.cpp
+++ Resoluções OJ/codeforces/Rounds/template.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 #define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#ifdef ONLINE_JUDGE
 #define endl '\n'
+#endif
 #define INF 0x3f3f3f3f
 #define MAX (1<<20)
+#define OUT MAX
 #define MOD 1000000007
 #define i64 long long
 #define all(x) (x).begin() , (x).end()
@@ -17,12 +20,17 @@
 #define vvii vector<vii>
 #define lsb(x) ((x) & (-x))
 #define gcd(x,y) __gcd((x),(y))
+#define esq(x) (x<<1)
+#define dir(x) ((x<<1)|1)
 #define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
 
 using namespace std;
 
-int t;
+int t, n;
 
 int main(){_
+    for (cin >> t; t--;){
+
+    }
     return 0;
 }
