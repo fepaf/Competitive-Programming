7ba84f78910e5247edf09252bf66f5b6d6dfc31d
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-05-29 11:34:43 -0400

    Problem 1640 - Sum of Two Values (Accepted)

---
+++ Resoluções OJ/cses/1640.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define OUT MAX
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define esq(x) (x<<1)
+#define dir(x) ((x<<1)|1)
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+
+using namespace std;
+
+/*
+Problem: Sum of Two Values
+Link: https://cses.fi/problemset/task/1643
+*/
+ 
+int n, x;
+ii a[MAX];
+
+int main(){_
+    cin >> n >> x;
+    for (int i=0; i<n; ++i){
+        cin >> a[i].fs;
+        a[i].sc = i+1;
+    }
+    sort(a, a+n);
+    for (int i=0, j=n-1; i<j; ){
+        if (a[i].fs+a[j].fs==x){
+            cout << a[i].sc << ' ' << a[j].sc << endl;
+            return 0;
+        } else if (a[i].fs+a[j].fs<x){
+            ++i;
+        } else {
+            --j;
+        }
+    }
+    cout << "IMPOSSIBLE" << endl;
+    return 0;
+}
