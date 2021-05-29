aa9ddd3df7749bbd230623c75fbccae678b7771f
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-05-29 10:53:49 -0400

    Problem 1643 - Maximum Subarray Sum

---
+++ Resoluções OJ/cses/1643.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+#define INF (1LL<<60)
+#define MAX (1<<20)
+#define OUT MAX
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define esq(x) (x<<1)
+#define dir(x) ((x<<1)|1)
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+
+using namespace std;
+
+/*
+Problem: Maximum Subarray Sum
+Link: https://cses.fi/problemset/task/1643
+*/
+ 
+int n;
+i64 ans, parcial, x;
+
+int main(){_
+    ans = -INF;
+    for (cin >> n; n--;){
+        cin >> x;
+        parcial = max(parcial+x, x);
+        ans = max(ans, parcial);
+    }
+    cout << ans << endl;
+    return 0;
+}
