dd5adfa00095fdd7d6b6303b459ac0b43ab9bcf6
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2021-06-03 12:46:29 -0400

    Problem 2185 - Prime Multiples (Accepted)

---
+++ Resoluções OJ/cses/2185.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+#define INF 0x3f3f3f3f
+#define MAX (1<<20)
+#define OUT MAX
+#define MOD 1000000007
+#define i64 long long
+#define all(x) (x).begin() , (x).end()
+#define sz(x) (int)(x).size()
+#define ii pair<int, int>
+#define fs first
+#define sc second
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<ii>
+#define vvii vector<vii>
+#define lsb(x) ((x) & (-x))
+#define gcd(x,y) __gcd((x),(y))
+#define esq(x) (x<<1)
+#define dir(x) ((x<<1)|1)
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+
+using namespace std;
+
+/*
+Problem: Prime Multiples
+Link: https://cses.fi/problemset/task/2185
+*/
+ 
+i64 n, k, a[20], x, q, ans;
+
+int main(){_
+    cin >> n >> k;
+    for (int i=0; i<k; ++i){
+        cin >> a[i];
+    }
+    ans = n;
+    for (int mask=0; mask<(1<<k); ++mask){
+        q = 0LL;
+        x = 1LL;
+        for (int i=0; i<k; ++i){
+            if (mask&(1<<i)){
+                x *= x <= n/a[i] ? a[i] : 0;
+                q ^= 1LL;
+            }
+        }
+        if (x) ans -= (q ? -n/x : n/x);
+    }
+    cout << ans << endl;
+    return 0;
+}
